719b8a1504e7ac9d05e40cd0d09f771120c1f98e
Adlerberg <alex.adlerberg@gmail.com>
2018-07-16 13:49:57 +0300

    Add getting ingredients from Database and error handling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,13 +22,17 @@ class BurgerBuilder extends Component {
         totalPrice: 3,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     }
 
     componentDidMount () {
         axios.get('https://burger-builder-b5adf.firebaseio.com/ingredients.json')
             .then (response => {
                 this.setState({ingredients: response.data})
+            })
+            .catch(error => {
+                this.setState({error: true})
             });
     }
     upadtePurchaseState (ingredients) {
@@ -114,14 +118,8 @@ class BurgerBuilder extends Component {
         for (let key in disableInfo) {
             disableInfo[key] = disableInfo[key] <= 0
         }
-        let orderSummary =  <OrderSummary ingredients={this.state.ingredients}
-        price={this.state.totalPrice} 
-        purchaseCanselled={this.purchaseCancelHandler}
-        purchaseContinued={this.purchaseContinueHandler}/>; 
-        if (this.state.loading) {
-            orderSummary = <Spinner />;
-        }
-        let burger = <Spinner />
+        let orderSummary = null;
+        let burger = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner />
         
         if (this.state.ingredients) {
             burger = (
@@ -136,8 +134,16 @@ class BurgerBuilder extends Component {
                     ordered={this.purchaseHandler} />
                 </Aux>
                 );
+                orderSummary =  <OrderSummary 
+                ingredients={this.state.ingredients}
+                price={this.state.totalPrice} 
+                purchaseCanselled={this.purchaseCancelHandler}
+                purchaseContinued={this.purchaseContinueHandler}/>; 
+        }    
+        if (this.state.loading) {
+            orderSummary = <Spinner />;
         }
-       
+
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
